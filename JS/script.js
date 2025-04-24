140b911c3a29c757f37d856555382ac445316f10
Ayomide Jeremiah <Ayomide Jeremiah>
2025-04-24 09:48:09 +0100

    add javascript functionality

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,1 @@
-$(document).ready(function () {
-    let display = $('#display');
 
-    $('.buttons button').on('click', function () {
-        let value = $(this).text();
-
-        if (value === 'C') {
-            display.val('');
-        } else if (value === '=') {
-            let result = eval(display.val());
-            display.val(result);
-        } else {
-            display.val(display.val() + value);
-        }
-    });
-});
